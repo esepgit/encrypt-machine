701cd64d995c805723ef6ac7166e0eaf608940b6
esepgit <150302936+esepgit@users.noreply.github.com>
2024-01-24 16:22:10 -0300

    details finished

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const encrypt = document.getElementById('encrypt');
 const decrypt = document.getElementById('decrypt');
 const input = document.getElementById('input-textarea');
 const output = document.getElementById('output');
+const copy = document.getElementById('btn-copy');
+const clear = document.getElementById('clear');
 
 encrypt.addEventListener('click', function() {
   const inputText = input.value;
@@ -38,21 +40,46 @@ encrypt.addEventListener('click', function() {
         result += inputText[i];
       }
     }
-    output.innerText = result;
+    output.value = result;
   }
   
 });
 
 decrypt.addEventListener('click', function() {
   const inputText = input.value;
-  let result = '';
-  result = inputText.replace('ai', 'a');
-  result = inputText.replace("enter", "e");
-  result = inputText.replace("ober", "o");
-  result = inputText.replace("imes", "i");
-  result = inputText.replace("ufat", "u");
- 
-  output.innerText = result;
+
+  if (/[^a-z\sñ]/g.test(inputText)) {
+    alert("Accents, symbols and capital letters are not allowed");
+  } else {
+
+    let result = input.value;
+    let aux = '';
+    result = inputText.replace(/ai/g, 'a');
+    aux = result.replace(/enter/g, "e");
+    result = aux.replace(/ober/g, "o");
+    aux = result.replace(/imes/g, "i");
+    result = aux.replace(/ufat/g, "u");
+  
+    output.value = result;
+  }
+});
+
+copy.addEventListener("click", async function() {
+   try {
+      await navigator.clipboard.writeText(output.value);
+      let aux = copy.innerHTML;
+      setTimeout(() => {
+        copy.innerHTML = aux;
+      }, 2000);
+      copy.innerHTML = "&#10003;";
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+});
+
+clear.addEventListener('click', function() {
+  input.value = '';
+  output.value = '';
 });
 
 
